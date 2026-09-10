b83feb9c4eed88209711656856a7c2cde6939222
Jaana Räsänen <jaanarasan@gmail.com>
2026-09-10 19:54:42 +0300

    toisen tunnin harjoitukset

---
+++ mod03/tuntiharjoite.py
@@ -0,0 +1,10 @@
+nimi = input("Anna nimesi ")
+
+if nimi == "Matti":
+    print("seuraava, kiitos!")
+
+else:
+    annos = int(input("Montako annosta?"))
+    annoshinta = float(annos) * 5.90
+    print("kokonaishinta on ", annos, 'euroa')
+print("seuraava, kiitos!")
