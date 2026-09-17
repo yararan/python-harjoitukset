d61507a6470844a7c1deb9015a098f647c4b8ccd
Jaana Räsänen <jaanarasan@gmail.com>
2026-09-17 12:38:42 +0300

    mod 04 tehtäviä tehty, osa tekemättä vielä

---
+++ mod04/tehtava01.py
@@ -0,0 +1,17 @@
+#alustetaan muuttujat 
+tavoiteluku = 1000
+numerot = 0
+
+#luodaan toteutus while-funktiolla
+while True:
+    numerot +=3 
+
+#luodaan lopettava ehto
+    if numerot >= tavoiteluku:
+        break
+    print("Tulostettu kolmella jaolliset numerot: ", numerot)
+
+
+#Tulostetaan tiedote, mitä juuri tehtiin
+print("Tuhat täynnä kolmella jaollisilla luvuilla")
+
---
+++ mod04/tehtava02.py
@@ -0,0 +1,5 @@
+muunnos = input("muutetaan syöttämäsi senttimetreiksi, anna tuumat: ")
+cm = (float(muunnos))
+tuumat = (float(2.54))
+tulos = tuumat * cm
+print("Antamasi tuumamäärä senttimetreinä ", tulos)
